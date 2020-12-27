2a5cff30c1fe4d61c824abfe9c42ba3dd37b7a7d
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-12-27 22:26:32 +0100

    Update ParseProg.hs
    - Added type definitions for the first part of the project.
    - Added function definitions for the first part of the project.

--- Parser/ParseProg.hs
+++ Parser/ParseProg.hs
@@ -3,11 +3,22 @@ module ParseProg where
 import Control.Applicative
 import Parser
 
-type Name = String
+type Name      = String
 type Program a = [ScDef a]
-type ScDef a = (Name, [a], Expr a)
-data Expr a = EVar a
-              deriving Show
+type ScDef a   = (Name, [a], Expr a)
+type Def a     = (a, Expr a)
+type Alter a   = (Int, [a], Expr a)
+
+data IsRec     = NonRecursive | Recursive
+                 deriving Show
+data Expr a    = EVar Name
+               | ENum Int
+               | EConstr Int Int
+               | EAp (Expr a) (Expr a)
+               | ELet IsRec [Def a] (Expr a)
+               | ECase (Expr a) [Alter a]
+               | ELam [a] (Expr a)
+                 deriving Show
 
 parseProg :: Parser (Program Name)
 parseProg = do function <- parseScDef
@@ -25,6 +36,15 @@ parseScDef = do name <- parseVar
 
 parseExpr :: Parser (Expr Name)
 parseExpr = do return (EVar "TODO")
+
+parseAExpr :: Parser (Expr Name)
+parseAExpr = do return (EVar "TODO")
+
+parseDef :: Parser (Def Name)
+parseDef = do return ("TODO", EVar "TODO")
+
+parseAlt :: Parser (Alter Name)
+parseAlt = do return (1, ["TODO"], EVar "TODO")
 
 parseVar :: Parser (String)
 parseVar = do return "TODO"
